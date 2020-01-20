5a04662b68a8932030d67f5c663ed09dcdb21075
ergemp <ergemp@gmail.com>
2020-01-20 11:31:48 +0300

    new spark session examples added

--- src/main/java/org/ergemp/training/spark/sql/session/NewContextFromExistingSession.java
+++ src/main/java/org/ergemp/training/spark/sql/session/NewContextFromExistingSession.java
@@ -1,4 +1,16 @@
 package org.ergemp.training.spark.sql.session;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
 public class NewContextFromExistingSession {
+    public static void main(String[] args){
+        SparkConf conf = new SparkConf()
+                .setAppName("appName")
+                .setMaster("local[*]");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        SparkSession sparkSession= new SparkSession(jsc.sc());
+    }
 }
